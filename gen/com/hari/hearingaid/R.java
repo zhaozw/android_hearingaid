5ac0ce284d4dc912eb6ad0ef4d0f0915875beeda
pharic <pharic.15@gmail.com>
2014-03-06 22:18:02 -0800

    Classification using Visualizer class

--- gen/com/hari/hearingaid/R.java
+++ gen/com/hari/hearingaid/R.java
@@ -11,12 +11,12 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int black=0x7f040004;
-        public static final int grey=0x7f040000;
-        public static final int orange1=0x7f040001;
-        public static final int orange2=0x7f040002;
-        public static final int orange3=0x7f040003;
-        public static final int white=0x7f040005;
+        public static final int black=0x7f050004;
+        public static final int grey=0x7f050000;
+        public static final int orange1=0x7f050001;
+        public static final int orange2=0x7f050002;
+        public static final int orange3=0x7f050003;
+        public static final int white=0x7f050005;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -25,20 +25,20 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090006;
-        public static final int button1=0x7f090003;
-        public static final int button2=0x7f090005;
-        public static final int editText1=0x7f090002;
-        public static final int listview=0x7f090000;
-        public static final int rel1=0x7f090001;
-        public static final int txtText=0x7f090004;
+        public static final int action_settings=0x7f0a0006;
+        public static final int button1=0x7f0a0003;
+        public static final int button2=0x7f0a0005;
+        public static final int editText1=0x7f0a0002;
+        public static final int listview=0x7f0a0000;
+        public static final int rel1=0x7f0a0001;
+        public static final int txtText=0x7f0a0004;
     }
     public static final class layout {
         public static final int activity_classify=0x7f030000;
@@ -46,19 +46,22 @@ public final class R {
         public static final int activity_tts__stt=0x7f030002;
     }
     public static final class menu {
-        public static final int classify=0x7f080000;
-        public static final int main=0x7f080001;
-        public static final int tts__stt=0x7f080002;
+        public static final int classify=0x7f090000;
+        public static final int main=0x7f090001;
+        public static final int tts__stt=0x7f090002;
+    }
+    public static final class raw {
+        public static final int car=0x7f040000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060001;
-        public static final int app_name=0x7f060000;
-        public static final int hello_world=0x7f060007;
-        public static final int text1=0x7f060002;
-        public static final int text2=0x7f060003;
-        public static final int title_activity_classify=0x7f060006;
-        public static final int title_activity_tts__stt=0x7f060005;
-        public static final int write=0x7f060004;
+        public static final int action_settings=0x7f070001;
+        public static final int app_name=0x7f070000;
+        public static final int hello_world=0x7f070007;
+        public static final int text1=0x7f070002;
+        public static final int text2=0x7f070003;
+        public static final int title_activity_classify=0x7f070006;
+        public static final int title_activity_tts__stt=0x7f070005;
+        public static final int write=0x7f070004;
     }
     public static final class style {
         /** 
@@ -82,10 +85,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
 }
--- src/com/hari/hearingaid/Classify.java
+++ src/com/hari/hearingaid/Classify.java
@@ -1,161 +1,230 @@
 package com.hari.hearingaid;
 
-import java.io.IOException;
-
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.media.MediaRecorder;
+import android.media.audiofx.Equalizer;
 import android.media.audiofx.Visualizer;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class Classify extends Activity {
-	private static final String LOG_TAG = "AudioRecordTest";
-	private static String mFileName = null;
-	private Visualizer mVis;
-	private RecordButton mRecordButton = null;
-	private MediaRecorder mRecorder = null;
+    private static final String TAG = "AudioFxDemo";
 
-	private PlayButton mPlayButton = null;
-	private MediaPlayer mPlayer = null;
+    private static final float VISUALIZER_HEIGHT_DIP = 50f;
 
-	private void onRecord(boolean start) {
-		if (start) {
-			startRecording();
-		} else {
-			stopRecording();
-		}
-	}
+    private MediaPlayer mMediaPlayer;
+    private Visualizer mVisualizer;
+    private Equalizer mEqualizer;
 
-	private void onPlay(boolean start) {
-		if (start) {
-			startPlaying();
-		} else {
-			stopPlaying();
-		}
-	}
+    private LinearLayout mLinearLayout;
+    private VisualizerView mVisualizerView;
+    private TextView mStatusTextView;
 
-	private void startPlaying() {
-		mPlayer = new MediaPlayer();
-		try {
-			mPlayer.setDataSource(mFileName);
-			mPlayer.prepare();
-			mPlayer.start();
-		} catch (IOException e) {
-			Log.e(LOG_TAG, "prepare() failed");
-		}
-	}
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
 
-	private void stopPlaying() {
-		mPlayer.release();
-		mPlayer = null;
-	}
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
 
-	private void startRecording() {
-		mRecorder = new MediaRecorder();
-		mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-		mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-		mRecorder.setOutputFile(mFileName);
-		mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        mStatusTextView = new TextView(this);
 
-		try {
-			mRecorder.prepare();
-		} catch (IOException e) {
-			Log.e(LOG_TAG, "prepare() failed");
-		}
+        mLinearLayout = new LinearLayout(this);
+        mLinearLayout.setOrientation(LinearLayout.VERTICAL);
+        mLinearLayout.addView(mStatusTextView);
 
-		mRecorder.start();
-	}
+        setContentView(mLinearLayout);
 
-	private void stopRecording() {
-		mRecorder.stop();
-		mRecorder.release();
-		mRecorder = null;
-	}
+        // Create the MediaPlayer
+        mMediaPlayer = MediaPlayer.create(this, R.raw.car);
+        Log.d(TAG, "MediaPlayer audio session ID: " + mMediaPlayer.getAudioSessionId());
 
-	class RecordButton extends Button {
-		boolean mStartRecording = true;
+        setupVisualizerFxAndUI();
+        setupEqualizerFxAndUI();
 
-		OnClickListener clicker = new OnClickListener() {
-			public void onClick(View v) {
-				onRecord(mStartRecording);
-				if (mStartRecording) {
-					setText("Stop recording");
-				} else {
-					setText("Start recording");
-				}
-				mStartRecording = !mStartRecording;
-			}
-		};
+        // Make sure the visualizer is enabled only when you actually want to receive data, and
+        // when it makes sense to receive data.
+        mVisualizer.setEnabled(true);
 
-		public RecordButton(Context ctx) {
-			super(ctx);
-			setText("Start recording");
-			setOnClickListener(clicker);
-		}
-	}
+        // When the stream ends, we don't need to collect any more data. We don't do this in
+        // setupVisualizerFxAndUI because we likely want to have more, non-Visualizer related code
+        // in this callback.
+        mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                mVisualizer.setEnabled(false);
+            }
+        });
 
-	class PlayButton extends Button {
-		boolean mStartPlaying = true;
+        mMediaPlayer.start();
+        mStatusTextView.setText("Playing audio...");
+    }
 
-		OnClickListener clicker = new OnClickListener() {
-			public void onClick(View v) {
-				onPlay(mStartPlaying);
-				if (mStartPlaying) {
-					setText("Stop playing");
-				} else {
-					setText("Start playing");
-				}
-				mStartPlaying = !mStartPlaying;
-			}
-		};
+    private void setupEqualizerFxAndUI() {
+        // Create the Equalizer object (an AudioEffect subclass) and attach it to our media player,
+        // with a default priority (0).
+        mEqualizer = new Equalizer(0, mMediaPlayer.getAudioSessionId());
+        mEqualizer.setEnabled(true);
 
-		public PlayButton(Context ctx) {
-			super(ctx);
-			setText("Start playing");
-			setOnClickListener(clicker);
-		}
-	}
+        TextView eqTextView = new TextView(this);
+        eqTextView.setText("Equalizer:");
+        mLinearLayout.addView(eqTextView);
 
-	public Classify() {
-		mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
-		mFileName += "/test.wav";
-	}
+        short bands = mEqualizer.getNumberOfBands();
 
-	@Override
-	public void onCreate(Bundle icicle) {
-		super.onCreate(icicle);
+        final short minEQLevel = mEqualizer.getBandLevelRange()[0];
+        final short maxEQLevel = mEqualizer.getBandLevelRange()[1];
 
-		LinearLayout ll = new LinearLayout(this);
-		mRecordButton = new RecordButton(this);
-		ll.addView(mRecordButton, new LinearLayout.LayoutParams(
-				ViewGroup.LayoutParams.WRAP_CONTENT,
-				ViewGroup.LayoutParams.WRAP_CONTENT, 0));
-		mPlayButton = new PlayButton(this);
-		ll.addView(mPlayButton, new LinearLayout.LayoutParams(
-				ViewGroup.LayoutParams.WRAP_CONTENT,
-				ViewGroup.LayoutParams.WRAP_CONTENT, 0));
-		setContentView(ll);
-		//mVis.setEnabled(true);
-	}
+        for (short i = 0; i < bands; i++) {
+            final short band = i;
 
-	@Override
-	public void onPause() {
-		super.onPause();
-		if (mRecorder != null) {
-			mRecorder.release();
-			mRecorder = null;
-		}
+            TextView freqTextView = new TextView(this);
+            freqTextView.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            freqTextView.setGravity(Gravity.CENTER_HORIZONTAL);
+            freqTextView.setText((mEqualizer.getCenterFreq(band) / 1000) + " Hz");
+            mLinearLayout.addView(freqTextView);
 
-		if (mPlayer != null) {
-			mPlayer.release();
-			mPlayer = null;
-		}
-	}
+            LinearLayout row = new LinearLayout(this);
+            row.setOrientation(LinearLayout.HORIZONTAL);
+
+            TextView minDbTextView = new TextView(this);
+            minDbTextView.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            minDbTextView.setText((minEQLevel / 100) + " dB");
+
+            TextView maxDbTextView = new TextView(this);
+            maxDbTextView.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            maxDbTextView.setText((maxEQLevel / 100) + " dB");
+
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
+            layoutParams.weight = 1;
+            SeekBar bar = new SeekBar(this);
+            bar.setLayoutParams(layoutParams);
+            bar.setMax(maxEQLevel - minEQLevel);
+            bar.setProgress(mEqualizer.getBandLevel(band));
+
+            bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                public void onProgressChanged(SeekBar seekBar, int progress,
+                        boolean fromUser) {
+                    mEqualizer.setBandLevel(band, (short) (progress + minEQLevel));
+                }
+
+                public void onStartTrackingTouch(SeekBar seekBar) {}
+                public void onStopTrackingTouch(SeekBar seekBar) {}
+            });
+
+            row.addView(minDbTextView);
+            row.addView(bar);
+            row.addView(maxDbTextView);
+
+            mLinearLayout.addView(row);
+        }
+    }
+
+    private void setupVisualizerFxAndUI() {
+        // Create a VisualizerView (defined below), which will render the simplified audio
+        // wave form to a Canvas.
+        mVisualizerView = new VisualizerView(this);
+        mVisualizerView.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                (int)(VISUALIZER_HEIGHT_DIP * getResources().getDisplayMetrics().density)));
+        mLinearLayout.addView(mVisualizerView);
+
+        // Create the Visualizer object and attach it to our media player.
+        mVisualizer = new Visualizer(mMediaPlayer.getAudioSessionId());
+        mVisualizer.setCaptureSize(Visualizer.getCaptureSizeRange()[1]);
+        mVisualizer.setDataCaptureListener(new Visualizer.OnDataCaptureListener() {
+            public void onWaveFormDataCapture(Visualizer visualizer, byte[] bytes,
+                    int samplingRate) {
+                mVisualizerView.updateVisualizer(bytes);
+            }
+
+            public void onFftDataCapture(Visualizer visualizer, byte[] bytes, int samplingRate) {}
+        }, Visualizer.getMaxCaptureRate() / 2, true, false);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (isFinishing() && mMediaPlayer != null) {
+            mVisualizer.release();
+            mEqualizer.release();
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
+    }
+}
+
+/**
+ * A simple class that draws waveform data received from a
+ * {@link Visualizer.OnDataCaptureListener#onWaveFormDataCapture }
+ */
+class VisualizerView extends View {
+    private byte[] mBytes;
+    private float[] mPoints;
+    private Rect mRect = new Rect();
+
+    private Paint mForePaint = new Paint();
+
+    public VisualizerView(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        mBytes = null;
+
+        mForePaint.setStrokeWidth(1f);
+        mForePaint.setAntiAlias(true);
+        mForePaint.setColor(Color.rgb(0, 128, 255));
+    }
+
+    public void updateVisualizer(byte[] bytes) {
+        mBytes = bytes;
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if (mBytes == null) {
+            return;
+        }
+
+        if (mPoints == null || mPoints.length < mBytes.length * 4) {
+            mPoints = new float[mBytes.length * 4];
+        }
+
+        mRect.set(0, 0, getWidth(), getHeight());
+
+        for (int i = 0; i < mBytes.length - 1; i++) {
+            mPoints[i * 4] = mRect.width() * i / (mBytes.length - 1);
+            mPoints[i * 4 + 1] = mRect.height() / 2
+                    + ((byte) (mBytes[i] + 128)) * (mRect.height() / 2) / 128;
+            mPoints[i * 4 + 2] = mRect.width() * (i + 1) / (mBytes.length - 1);
+            mPoints[i * 4 + 3] = mRect.height() / 2
+                    + ((byte) (mBytes[i + 1] + 128)) * (mRect.height() / 2) / 128;
+        }
+
+        canvas.drawLines(mPoints, mForePaint);
+    }
 }
